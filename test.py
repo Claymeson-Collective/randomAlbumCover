bee3407c4b0ad6686fcc180cbb4b43c6fbbb96d3
Parry <2393265p@student.gla.ac.uk>
2020-03-28 15:30:47 +0000

    Update Test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
 driver.get('https://en.wikipedia.org/wiki/Special:Random')
-randomTitle = (driver.find_elements_by_name('firstHeading')[0]).text
+randomHeading = driver.find_elements_by_name('firstHeading')[0]
+randomTitle = randomHeading.text
 
 driver.quit()
