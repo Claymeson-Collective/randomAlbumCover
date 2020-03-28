cbf5178de95966f5f4b5dd75b689fbde7e3c7add
Parry <2393265p@student.gla.ac.uk>
2020-03-28 16:14:37 +0000

    removed .idea files

--- Scrapers/getImage.py
+++ Scrapers/getImage.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 def getimage():
     driver = webdriver.Chrome('chromedriver.exe')
     driver.get('https://picsum.photos/800')
-    img = driver.
+    img = driver.current_url
     driver.quit()
     return src
 
